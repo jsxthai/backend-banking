68aeee32864c0b1634114b4f77aa65997f25a2de
Thai Pham <jsxthai@gmail.com>
2020-11-15 20:48:07 +0700

    setup basic

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 7777;
+
+// connect mongodb
+const mongoDBUrl = process.env.MONGODB_URL || 'mongodb://localhost/banking'
+try {
+    mongoose.connect(mongoDBUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }, () => console.log("mongodb connected"));
+} catch (error) {
+    console.log("mongodb could not connect");
+}
+
+// app use
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('tiny'));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
