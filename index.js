86b7c7c038cacbb18439bf0bb1d5bd012c12766b
Thai Pham <jsxthai@gmail.com>
2020-11-15 23:27:13 +0700

    del route /

--- index.js
+++ index.js
@@ -26,8 +26,5 @@ app.use(morgan('tiny'));
 // use router
 app.use('/api/users', users)
 
-app.get('/', (req, res) => {
-    res.json('hello, i am api banking');
-})
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
