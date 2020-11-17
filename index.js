5917b054251935500334269605474dc971e23e99
Thai Pham <jsxthai@gmail.com>
2020-11-17 15:58:01 +0700

    use cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import users from './routers/users.js';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
@@ -20,6 +21,7 @@ try {
 }
 
 // app use
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('tiny'));
