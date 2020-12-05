fb2dda5c08636e2e5fffbd954ca2066a8adbaf04
thai pham <jsxthai@gmail.com>
2020-12-05 19:29:30 +0700

    query history $or

--- controllers/historyTrans.js
+++ controllers/historyTrans.js
@@ -16,44 +16,62 @@ export const historyTrans = async (req, res) => {
     if (!accountNumber)
         return res.status(400).json({ msg: "No account number" });
 
+    // user là người tự chuyển tiền, accountSource
     const filter = {
         accountSource: accountNumber,
     };
+    // user là người được nhận tiền, accountDest
+    const filter2 = {
+        accountDest: accountNumber,
+    };
+
     // hiện tại chỉ có 2 type
     // nhiều: array, true find in array
     if (typeTrans === "receive" || typeTrans === "transfer") {
         filter.typeTrans = typeTrans;
+        filter2.typeTrans = typeTrans;
     }
 
     // làm chung, tiện cho client, server mỗi lần phải get length lại
     // get lại đảm bảo length luôn được cập nhật mới
-    const lengthModel = await getLengthModel(Trans, filter);
+    const lengthModelAccountSource = await getLengthModel(Trans, filter);
+    const lengthModelAccountDest = await getLengthModel(Trans, filter2);
+
     // console.log("leng", lengthModel);
-    if (lengthModel === false) {
+    if (
+        lengthModelAccountSource === false ||
+        lengthModelAccountDest === false
+    ) {
         return res
             .status(400)
             .json({ msg: "Server get total lenght of model err" });
     }
 
     try {
-        await Trans.find(filter, { sign: 0, _id: 0 }, (err, trans) => {
-            if (err)
-                return res
-                    .status(400)
-                    .json({ msg: "Err find transaction" }, err);
-            if (trans.length < 1)
-                return res.status(404).json({
-                    msg: "Account no have transaction, check again",
-                });
+        await Trans.find(
+            // { $and: [filter, filter2] },
+            { $or: [filter, filter2] },
+            { sign: 0, _id: 0 },
+            (err, trans) => {
+                if (err)
+                    return res
+                        .status(400)
+                        .json({ msg: "Err find transaction" }, err);
+                // if (trans.length < 1)
+                //     return res.status(404).json({
+                //         msg: "Account no have transaction, check again",
+                //     });
 
-            return res.status(200).json({
-                msg: "Find transaction success",
-                trans,
-                totalRows: lengthModel,
-                limit,
-                page,
-            });
-        })
+                return res.status(200).json({
+                    msg: "Find transaction success",
+                    trans,
+                    totalRows:
+                        lengthModelAccountSource + lengthModelAccountDest,
+                    limit,
+                    page,
+                });
+            }
+        )
             .skip(page > 0 ? (page - 1) * limit : 0)
             .limit(limit)
             .sort({ date: "desc" });
